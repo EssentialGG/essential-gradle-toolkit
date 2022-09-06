921ed23cd657fd86c78051b0797631f08eaba617
Jonas Herzig <jonas@spark-squared.com>
2022-09-06 15:05:51 +0200

    multi-version: Add default yarn and forge version for MC 1.19.2

--- src/main/kotlin/gg/essential/defaults/loom.gradle.kts
+++ src/main/kotlin/gg/essential/defaults/loom.gradle.kts
@@ -19,6 +19,7 @@ val revisions = mutableListOf<Revision>()
 
 revisions.add(Revision(
     yarn = mapOf(
+        11902 to "1.19.2+build.9:v2",
         11901 to "1.19.1+build.5:v2",
         11900 to "1.19+build.1:v2",
         11802 to "1.18.2+build.2:v2",
@@ -48,6 +49,7 @@ revisions.add(Revision(
     ),
     fabricLoader = "0.13.3",
     forge = mapOf(
+        11902 to "1.19.2-43.1.16",
         11900 to "1.19-41.0.63",
         11801 to "1.18.1-39.0.79",
         11701 to "1.17.1-37.0.112",
