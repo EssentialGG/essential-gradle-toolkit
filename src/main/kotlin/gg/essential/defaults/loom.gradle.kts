6f45d9b24936a670109d68ac3ce58aca4ddf75c2
David Senoner <david.senoner@student.tugraz.at>
2023-01-11 23:48:26 +0100

    loom: Update most Forge, Fabric Loader, MCP and Yarn versions
    For Forge and MCP, this should be the latest recommended builds (except on
    1.12.2 where the latest stable Forge is incompatible with archloom).
    For Fabric and Yarn, it's the latest versions.
    
    Co-authored-by: Jonas Herzig <jonas@spark-squared.com>

--- src/main/kotlin/gg/essential/defaults/loom.gradle.kts
+++ src/main/kotlin/gg/essential/defaults/loom.gradle.kts
@@ -14,7 +14,19 @@ data class Revision(
     val mcp: Map<Int, String>,
     val fabricLoader: String,
     val forge: Map<Int, String>,
-)
+) {
+    fun update(
+        yarn: Map<Int, String> = emptyMap(),
+        mcp: Map<Int, String> = emptyMap(),
+        fabricLoader: String = this.fabricLoader,
+        forge: Map<Int, String> = emptyMap(),
+    ) = Revision(
+        this.yarn + yarn,
+        this.mcp + mcp,
+        fabricLoader,
+        this.forge + forge,
+    )
+}
 val revisions = mutableListOf<Revision>()
 
 revisions.add(Revision(
@@ -68,6 +80,51 @@ revisions.add(Revision(
         10809 to "1.8.9-11.15.1.2318-1.8.9",
         10800 to "1.8-11.14.4.1563",
         10710 to "1.7.10-10.13.4.1558-1.7.10",
+    ),
+))
+
+revisions.add(revisions.last().update(
+    yarn = mapOf(
+        11903 to "1.19.3+build.5:v2",
+        11902 to "1.19.2+build.28:v2",
+        11901 to "1.19.1+build.6:v2",
+        11900 to "1.19+build.4:v2",
+        11802 to "1.18.2+build.4:v2",
+        11701 to "1.17.1+build.65:v2",
+        11604 to "1.16.4+build.9:v2",
+        11602 to "1.16.2+build.47:v2",
+        11601 to "1.16.1+build.21:v2",
+        11502 to "1.15.2+build.17",
+        11404 to "1.14.4+build.18",
+    ),
+    mcp = mapOf(
+        11502 to "stable:60-1.15",
+        11404 to "stable:58-1.14.4",
+        11202 to "stable:39-1.12",
+        11201 to "stable:39-1.12",
+        11200 to "stable:39-1.12",
+        11102 to "stable:32-1.11",
+        11100 to "stable:32-1.11",
+        11002 to "stable:29-1.10.2",
+        10809 to "stable:22-1.8.9",
+        10800 to "stable:18-1.8",
+    ),
+    fabricLoader = "0.14.2",
+    forge = mapOf(
+        11903 to "1.19.3-44.1.0",
+        11902 to "1.19.2-43.2.0",
+        11900 to "1.19-41.1.0",
+        11801 to "1.18.1-39.1.0",
+        11701 to "1.17.1-37.1.1",
+        11502 to "1.15.2-31.2.57",
+        11404 to "1.14.4-28.2.26",
+        11202 to "1.12.2-14.23.5.2847", // newer versions use a different build system
+        11201 to "1.12.1-14.22.1.2478",
+        11102 to "1.11.2-13.20.1.2588",
+        11100 to "1.11-13.19.1.2189",
+        11002 to "1.10.2-12.18.3.2511",
+        10904 to "1.9.4-12.17.0.2317",
+        10710 to "1.7.10-10.13.4.1614-1.7.10",
     ),
 ))
 
