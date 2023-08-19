0d335cefad377df427c209e1d36afba73e8d400d
sychic <47618543+Sychic@users.noreply.github.com>
2023-08-19 14:02:03 -0400

    change: remove unecessary manifest attribute

--- src/main/kotlin/gg/essential/essential-loader.gradle.kts
+++ src/main/kotlin/gg/essential/essential-loader.gradle.kts
@@ -20,7 +20,6 @@ when {
         tasks.named<Jar>("jar") {
             dependsOn(essentialLoader)
             manifest.attributes(mapOf(
-                "FMLCorePluginContainsMod" to "Yes, yes it does",
                 "TweakClass" to "gg.essential.loader.stage0.EssentialSetupTweaker"
             ))
             from({ zipTree(essentialLoader.singleFile) })
