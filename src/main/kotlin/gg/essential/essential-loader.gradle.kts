60b8a4f5664dff3e40d419610cf4a31e671a945f
sychic <47618543+Sychic@users.noreply.github.com>
2023-11-23 11:49:27 -0500

    loader: error on unsupported versions

--- src/main/kotlin/gg/essential/essential-loader.gradle.kts
+++ src/main/kotlin/gg/essential/essential-loader.gradle.kts
@@ -32,32 +32,34 @@ when {
         }
     }
 
+    //FIXME: Fix loader not working on ml
     platform.isModLauncher -> {
-        dependencies {
-            val relocatedPackage = findProperty("essential.loader.package")?.toString() ?: throw GradleException("""
-                A package for the Essential loader to be relocated to has not been set.
-                You need to set `essential.loader.package` in the project's `gradle.properties` file to a package where Essential's loader will be relocated to.
-                For example: `essential.loader.package = org.example.coolmod.relocated.essential`
-            """.trimIndent())
-            val relocationAttribute =
-                registerRelocationAttribute("essential-loader-relocated") {
-                    relocate("gg.essential.loader.stage0", "$relocatedPackage.stage0")
-                    // preserve stage1 path
-                    rename("gg/essential/loader/stage0/stage1.jar", "gg/essential/loader/stage0/stage1.jar")
-                }
-            essentialLoader.attributes {
-                attribute(relocationAttribute, true)
-            }
-            if (platform.mcVersion < 11700) {
-                "forgeRuntimeLibrary"(essentialLoader("gg.essential:loader-modlauncher8:1.2.2")!!)
-            } else {
-                "forgeRuntimeLibrary"(essentialLoader("gg.essential:loader-modlauncher9:1.2.2")!!)
-            }
-        }
-        tasks.named<Jar>("jar") {
-            dependsOn(essentialLoader)
-            from({ zipTree(essentialLoader.singleFile) })
-        }
+        error("Modlauncher is currently not supported")
+//        dependencies {
+//            val relocatedPackage = findProperty("essential.loader.package")?.toString() ?: throw GradleException("""
+//                A package for the Essential loader to be relocated to has not been set.
+//                You need to set `essential.loader.package` in the project's `gradle.properties` file to a package where Essential's loader will be relocated to.
+//                For example: `essential.loader.package = org.example.coolmod.relocated.essential`
+//            """.trimIndent())
+//            val relocationAttribute =
+//                registerRelocationAttribute("essential-loader-relocated") {
+//                    relocate("gg.essential.loader.stage0", "$relocatedPackage.stage0")
+//                    // preserve stage1 path
+//                    rename("gg/essential/loader/stage0/stage1.jar", "gg/essential/loader/stage0/stage1.jar")
+//                }
+//            essentialLoader.attributes {
+//                attribute(relocationAttribute, true)
+//            }
+//            if (platform.mcVersion < 11700) {
+//                "forgeRuntimeLibrary"(essentialLoader("gg.essential:loader-modlauncher8:1.2.2")!!)
+//            } else {
+//                "forgeRuntimeLibrary"(essentialLoader("gg.essential:loader-modlauncher9:1.2.2")!!)
+//            }
+//        }
+//        tasks.named<Jar>("jar") {
+//            dependsOn(essentialLoader)
+//            from({ zipTree(essentialLoader.singleFile) })
+//        }
     }
 
     else -> error("No loader available for this platform")
