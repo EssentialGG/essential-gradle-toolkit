673a8861cc2661a694d90009cd052112254da7b3
DJtheRedstoner <52044242+DJtheRedstoner@users.noreply.github.com>
2024-05-10 10:24:29 -0400

    api-validation: Fix applying to subprojects that are not MC projects
    GitHub: #15

--- src/main/kotlin/gg/essential/gradle/util/utils.kt
+++ src/main/kotlin/gg/essential/gradle/util/utils.kt
@@ -3,6 +3,7 @@ package gg.essential.gradle.util
 import kotlinx.metadata.jvm.JvmMetadataVersion
 import org.gradle.api.GradleException
 import org.gradle.api.JavaVersion
+import org.gradle.api.Project
 import java.util.Calendar
 import java.util.GregorianCalendar
 
@@ -30,3 +31,9 @@ internal fun compatibleKotlinMetadataVersion(version: IntArray): JvmMetadataVers
 // A safe, constant value for creating consistent zip entries
 // From: https://github.com/gradle/gradle/blob/d6c7fd470449a59fc57a26b4ebc0ad83c64af50a/subprojects/core/src/main/java/org/gradle/api/internal/file/archive/ZipCopyAction.java#L42-L57
 val CONSTANT_TIME_FOR_ZIP_ENTRIES = GregorianCalendar(1980, Calendar.FEBRUARY, 1, 0, 0, 0).timeInMillis
+
+val Project.isMultiversionChildProject
+    get() = plugins.hasPlugin("gg.essential.multi-version")
+
+val Project.multiversionChildProjects
+    get() = childProjects.filter { it.value.isMultiversionChildProject }
--- src/main/kotlin/gg/essential/multi-version.api-validation.gradle.kts
+++ src/main/kotlin/gg/essential/multi-version.api-validation.gradle.kts
@@ -2,6 +2,8 @@ package gg.essential
 
 import gg.essential.gradle.multiversion.apivalidation.ExtractApiFile
 import gg.essential.gradle.multiversion.apivalidation.MergeApiFiles
+import gg.essential.gradle.util.isMultiversionChildProject
+import gg.essential.gradle.util.multiversionChildProjects
 import org.gradle.api.tasks.Sync
 
 plugins {
@@ -13,24 +15,26 @@ val mergedApiFile = file("api/$name.api")
 val apiDump by tasks.registering(MergeApiFiles::class) {
     output.set(mergedApiFile)
     evaluationDependsOnChildren()
-    inputs.from(subprojects.map { project ->
+    inputs.from(multiversionChildProjects.map { (_, project) ->
         project.tasks.named("apiDump").map { project.file("api/${project.name}.api") }
     })
 }
 
 subprojects {
-    val projectApiDir = project.file("api").also { it.mkdirs() }
-    val extractApiDefinition by tasks.registering(ExtractApiFile::class) {
-        selector.set(project.name)
-        input.set(mergedApiFile)
-        output.set(projectApiDir.resolve("${project.name}.api"))
-    }
-    afterEvaluate {
-        tasks.named("apiCheck") {
-            dependsOn(extractApiDefinition)
+    pluginManager.withPlugin("gg.essential.multi-version") {
+        val projectApiDir = project.file("api").also { it.mkdirs() }
+        val extractApiDefinition by tasks.registering(ExtractApiFile::class) {
+            selector.set(project.name)
+            input.set(mergedApiFile)
+            output.set(projectApiDir.resolve("${project.name}.api"))
         }
-        tasks.named<Sync>("apiDump") {
-            finalizedBy(apiDump)
+        afterEvaluate {
+            tasks.named("apiCheck") {
+                dependsOn(extractApiDefinition)
+            }
+            tasks.named<Sync>("apiDump") {
+                finalizedBy(apiDump)
+            }
         }
     }
 }
