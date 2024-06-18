45c0d402984f28a465bcfc2f9e689c2e6911bd5a
sychic <47618543+Sychic@users.noreply.github.com>
2024-06-18 15:17:01 -0700

    loader: re-enable support for modlauncher platforms

--- src/main/kotlin/gg/essential/essential-loader.gradle.kts
+++ src/main/kotlin/gg/essential/essential-loader.gradle.kts
@@ -34,32 +34,51 @@ when {
 
     //FIXME: Fix loader not working on ml
     platform.isModLauncher -> {
-        error("Modlauncher is currently not supported")
-//        dependencies {
-//            val relocatedPackage = findProperty("essential.loader.package")?.toString() ?: throw GradleException("""
-//                A package for the Essential loader to be relocated to has not been set.
-//                You need to set `essential.loader.package` in the project's `gradle.properties` file to a package where Essential's loader will be relocated to.
-//                For example: `essential.loader.package = org.example.coolmod.relocated.essential`
-//            """.trimIndent())
-//            val relocationAttribute =
-//                registerRelocationAttribute("essential-loader-relocated") {
-//                    relocate("gg.essential.loader.stage0", "$relocatedPackage.stage0")
-//                    // preserve stage1 path
-//                    rename("gg/essential/loader/stage0/stage1.jar", "gg/essential/loader/stage0/stage1.jar")
-//                }
-//            essentialLoader.attributes {
-//                attribute(relocationAttribute, true)
-//            }
-//            if (platform.mcVersion < 11700) {
-//                "forgeRuntimeLibrary"(essentialLoader("gg.essential:loader-modlauncher8:1.2.2")!!)
-//            } else {
-//                "forgeRuntimeLibrary"(essentialLoader("gg.essential:loader-modlauncher9:1.2.2")!!)
-//            }
-//        }
-//        tasks.named<Jar>("jar") {
-//            dependsOn(essentialLoader)
-//            from({ zipTree(essentialLoader.singleFile) })
-//        }
+        val relocatedPackage = findProperty("essential.loader.package")?.toString() ?: throw GradleException("""
+                A package for the Essential loader to be relocated to has not been set.
+                You need to set `essential.loader.package` in the project's `gradle.properties` file to a package where Essential's loader will be relocated to.
+                For example: `essential.loader.package = org.example.coolmod.relocated.essential`
+            """.trimIndent())
+        dependencies {
+            val relocationAttribute =
+                registerRelocationAttribute("essential-loader-relocated") {
+                    relocate("gg.essential.loader.stage0", "$relocatedPackage.stage0")
+                    // preserve stage1 path
+                    rename("gg/essential/loader/stage0/stage1.jar", "gg/essential/loader/stage0/stage1.jar")
+                }
+            essentialLoader.attributes {
+                attribute(relocationAttribute, true)
+            }
+            if (platform.mcVersion < 11700) {
+                "forgeRuntimeLibrary"(essentialLoader("gg.essential:loader-modlauncher8:1.2.2")!!)
+            } else {
+                "forgeRuntimeLibrary"(essentialLoader("gg.essential:loader-modlauncher9:1.2.2")!!)
+            }
+        }
+        tasks {
+            named<Jar>("jar") {
+                dependsOn(essentialLoader)
+                from({ zipTree(essentialLoader.singleFile) })
+            }
+            register("generateEssentialLoaderMixinConfig") {
+                val outputFile = file("${layout.buildDirectory}/generated-resources/mixin.stage0.essential-loader.json")
+                outputs.file(outputFile)
+                doLast {
+                    outputFile.writeText("""
+                        {
+                          "minVersion": "0.8",
+                          "compatibilityLevel": "JAVA_16",
+                          "plugin": "$relocatedPackage.stage0.EssentialStage0MixinPlugin",
+                          "package" : "$relocatedPackage.stage0"
+                        }
+                    """.trimIndent())
+                }
+            }
+            named<ProcessResources>("processResources") {
+                dependsOn(named("generateEssentialLoaderMixinConfig"))
+                from(file("${layout.buildDirectory}/generated-resources"))
+            }
+        }
     }
 
     else -> error("No loader available for this platform")
