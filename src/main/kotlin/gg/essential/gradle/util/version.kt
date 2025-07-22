b566e0e1dbbc4cdcc480bca51c1a8a3554fc1f47
Jonas Herzig <jonas@spark-squared.com>
2025-07-22 09:30:23 +0200

    misc: Replace deprecated method usage
    Enables use of configuration cache.

--- src/main/kotlin/gg/essential/gradle/util/version.kt
+++ src/main/kotlin/gg/essential/gradle/util/version.kt
@@ -13,13 +13,18 @@ fun Project.versionFromBuildIdAndBranch(): String {
 
 private fun Project.buildId(): String? = project.properties["BUILD_ID"]?.toString()
 
-private fun Project.branch(): String = project.properties["branch"]?.toString() ?: try {
-    val stdout = java.io.ByteArrayOutputStream()
-    exec {
+private fun Project.branch(): String = project.properties["branch"]?.toString() ?:
+    providers.exec {
         commandLine("git", "rev-parse", "--abbrev-ref", "HEAD")
-        standardOutput = stdout
+    }.let { execOutput ->
+        execOutput.result.flatMap { result ->
+            if (result.exitValue == 0) {
+                execOutput.standardOutput.asText.map { it.trim() }
+            } else {
+                provider { "unknown" }
+            }
+        }
     }
-    stdout.toString().trim()
-} catch (e: Throwable) {
-    "unknown"
-}
+        // FIXME should ideally not read this here because it makes the configuration cache less effective
+        //  but `version` doesn't yet support `Provider`: https://github.com/gradle/gradle/issues/13672
+        .get()
